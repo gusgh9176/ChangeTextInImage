d6247d7cc963b8772898e52c8ecdeeb0e825b5de
eui-space <hibyby2000@naver.com>
2019-04-28 15:55:21 +0900

    후보군추출모듈 + 겹치는 부분 합치기 기능
    아직 코드를 이용하여 나온 결과 사진들 분류는 안함.

--- target_img_extract_module/SaveImage.py
+++ target_img_extract_module/SaveImage.py
@@ -14,6 +14,7 @@ class rectang:
     y1=0
     x2=0
     y2=0
+    live=1
     def __init__(self, x1, y1, x2, y2):
         self.x1=x1
         self.y1=y1
@@ -59,7 +60,7 @@ def change1(img):
 #폴더내의 이미지 배열로 불러옴
 i=1
 j=1
-path_dir = './Image/oriImage'
+path_dir = '../Image/oriImage2/'
 file_list = os.listdir(path_dir)
 # 폴더에 있는 이미지를 전부 잘라서 저장함
 number = 1
@@ -69,9 +70,11 @@ for cutImage in file_list:
     image_List2=[]
     #rectangle 배열
     rect_List = []
+    rect_List2=[]
+    rect_List3=[]
     print(cutImage)
     #이미지 변수에 저장        
-    path = 'Image/oriImage'
+    path = '../Image/oriImage2/'
     src = cv2.imread(path+cutImage, cv2.IMREAD_UNCHANGED)
     
     #drawContours 가 원본이미지를 변경하기에 이미지 복사
@@ -99,41 +102,59 @@ for cutImage in file_list:
         #rectangle 좌표들 배열에 저장
         rect_List.append(rectang(x, y, x+w, y+h))
 
-    #사각형 내부의 사각형 제거. 가로,세로 좌표가 다른 사각형 내부에 포함되면 그려지지않게함
+    height , width, channel = src.shape
     for r1 in rect_List:
-        switch = True
-        for r2 in rect_List:
-            if ((rect_List[r1].x1 < rect_List[r2].x1) and (rect_List[r1].x2 >= rect_List[r2].x2)):
-                if ((rect_List[r1].y1 < rect_List[r2].y1) and (rect_List[r1].y2 >= rect_List[r2].y2)):
-                    continue
+        if(((r1.x2-r1.x1)<width*0.4)and((r1.y2-r1.y1)<height*0.4)):
+            rect_List2.append(rectang(r1.x1, r1.y1, r1.x2, r1.y2))
+
+    #사각형 내부의 사각형 제거. 가로,세로 좌표가 다른 사각형 내부에 포함되면 그려지지않게함
+    for r1 in rect_List2:
+        check=0
+        if(r1.live==1):
+            for r2 in rect_List2:
+                if(r2.live==1):
+                    if (((r1.x1 < r2.x1) and (r2.x1 < r1.x2)) or ((r1.x1 < r2.x2)and (r2.x2< r1.x2))):
+                        if (((r1.y1 < r2.y1) and (r2.y1 < r1.y2)) or ((r1.y1 < r2.y2)and (r2.y2< r1.y2))):
+                            r2.live=0
+                            r1.x1=min(r1.x1, r2.x1)
+                            r1.y1=min(r1.y1, r2.y1)
+                            r1.x2=max(r1.x2, r2.x2)
+                            r1.y2=max(r1.y2, r2.y2)
+                            check=1
+    for r1 in rect_List2:
+        if (r1.live == 1):
+            rect_List3.append(rectang(r1.x1, r1.y1, r1.x2, r1.y2))
         #해당 될시 그리는부분 스킵
-        if(switch):
-            img2 = cv2.rectangle(img2,(r1.x1, r1.y1),(r1.x2, r1.y2),(0,255,0),1)
+
+
+    for r1 in rect_List3:
+        img2 = cv2.rectangle(img2,(r1.x1, r1.y1),(r1.x2, r1.y2),(0,255,0),1)
     
-            #배열에 텐서플로우에 전달할 이미지 저장
-            dst = src.copy()
-            dst = src[r1.y1:r1.y2, r1.x1:r1.x2]
-            dst_gray = cv2.cvtColor(dst,cv2.COLOR_BGR2GRAY)
-            dst_ret, dst_gray = cv2.threshold(dst_gray, 127,255,cv2.THRESH_BINARY)
-            dst_laplacian = cv2.Laplacian(dst_gray, cv2.CV_8U)
-            dst_laplacian = cv2.resize(dst_laplacian, dsize=(100, 100), interpolation=cv2.INTER_AREA)
-            image_List.append(dst_gray)
-            image_List2.append(dst_laplacian)
+        #배열에 텐서플로우에 전달할 이미지 저장
+        dst = src.copy()
+        dst = src[r1.y1:r1.y2, r1.x1:r1.x2]
+        dst_gray = cv2.cvtColor(dst,cv2.COLOR_BGR2GRAY)
+        dst_ret, dst_gray = cv2.threshold(dst_gray, 127,255,cv2.THRESH_BINARY)
+        dst_laplacian = cv2.Laplacian(dst_gray, cv2.CV_8U)
+        dst_laplacian = cv2.resize(dst_laplacian, dsize=(100, 100), interpolation=cv2.INTER_AREA)
+        image_List.append(dst)
+        image_List2.append(dst_laplacian)
 
     for passImage in image_List:
         name = "image"
         #자른 이미지 저장@@
-        path='image/trainImage/'+name+str(i)
+        path='../text_recog_module/negative/'+name+str(i)
         cv2.imwrite(path+'.jpg', passImage)
         i=i+1
 
-    for passImage in image_List2:
+    '''
+    for passImage in image_List4:
         name = "L_image"
         # 자른 이미지 저장@@
         path = 'image/trainImage2/' + name + str(i)
         cv2.imwrite(path + '.jpg', passImage)
         j = j + 1
-
+    '''
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
