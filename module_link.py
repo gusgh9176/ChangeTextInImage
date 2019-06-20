2965f643099bd891ec2e471c204049d3735b1e36
Eui Ryeong <37531737+Deplim@users.noreply.github.com>
2019-06-20 18:06:49 +0900

    정리

--- module_link.py
+++ module_link.py
@@ -1,66 +1,63 @@
-
 # coding: utf-8
 
 # In[41]:
-
-
 import math
 import cv2
 import numpy as np
 import imutils
 import os
 from keras.models import model_from_json
-from PIL import Image     #pip install pillow
-from pytesseract import * #pip install pytesseract
+from PIL import Image  # pip install pillow
+from pytesseract import *  # pip install pytesseract
 import configparser
 from googletrans import Translator
 
-translator=Translator()
+translator = Translator()
 
-# In[87]:
-
-
-#사각형
+# 사각형
 class rectang:
-    x1=0
-    y1=0
-    x2=0
-    y2=0
+    x1 = 0
+    y1 = 0
+    x2 = 0
+    y2 = 0
     live = 1
-    def __init__(self, x1, y1, x2, y2):
-        self.x1=x1
-        self.y1=y1
-        self.x2=x2
-        self.y2=y2
 
-#허프 변환
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
+
+# 허프 변환
 def removeVerticalLines(img, limit):
     lines = None
     threshold = 100
     minLength = 60
     lineGap = 10
     rho = 1
-    lines = cv2.HoughLinesP(img, rho, np.pi/180, threshold, minLength, lineGap)
-    if(lines is not None):   # lines  이 비지 않았을때만 실행한다.
+    lines = cv2.HoughLinesP(img, rho, np.pi / 180, threshold, minLength, lineGap)
+    if (lines is not None):  # lines  이 비지 않았을때만 실행한다.
         for i in range(len(lines)):
             for x1, y1, x2, y2 in lines[i]:
-                gapY = abs(y2-y1)
-                gapX = abs(x2-x1)
-                if(gapY>limit or gapX>limit and limit>0):
-                    cv2.line(img, (x1,y1), (x2,y2), (0, 0, 0), 3)
+                gapY = abs(y2 - y1)
+                gapX = abs(x2 - x1)
+                if (gapY > limit or gapX > limit and limit > 0):
+                    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 0), 3)
+
 
 def change1(img):
     temp_img = img.copy()
-    temp_img = cv2.bilateralFilter(temp_img,9,75,75)
-    #노이즈 제거 위한 커널(erode)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+    temp_img = cv2.bilateralFilter(temp_img, 9, 75, 75)
+    # 노이즈 제거 위한 커널(erode)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
     erode = cv2.erode(temp_img, kernel, iterations=1)
-    #이미지 grayscale
+    # 이미지 grayscale
     gray = cv2.cvtColor(temp_img, cv2.COLOR_BGR2GRAY)
-    #global 이진화
-    ret1, th = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
-    canny = cv2.Canny(th,180,250, apertureSize = 5)
-    #직선 제거
+    # global 이진화
+    ret1, th = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+    canny = cv2.Canny(th, 180, 250, apertureSize=5)
+    # 직선 제거
     removeVerticalLines(canny, 70)
     return canny
 
@@ -196,68 +193,66 @@ def combineRectang(rect_List):
 # In[88]:
 
 
-#불러올 이미지 주소 가져오기
+# 불러올 이미지 주소 가져오기
 path = './image/Spider_Man/'
 f_Name = 'Spider-Man - Far From Home Prelude 02 (of 02)-012.jpg'
 
 # In[89]:
 
 
-#텐서플로우에 전달할 이미지를 저장할 배열
-image_List=[]
-#rectangle 배열
+# 텐서플로우에 전달할 이미지를 저장할 배열
+image_List = []
+# rectangle 배열
 rect_List = []
 
-#이미지 변수에 저장
-src = cv2.imread(path+f_Name, cv2.IMREAD_UNCHANGED)
+# 이미지 변수에 저장
+src = cv2.imread(path + f_Name, cv2.IMREAD_UNCHANGED)
 
-#drawContours 가 원본이미지를 변경하기에 이미지 복사
-img1 = src.copy() #처음 Contours 그려짐
-img2 = src.copy() #Rectangle Contours 그려짐
-
+# drawContours 가 원본이미지를 변경하기에 이미지 복사
+img1 = src.copy()  # 처음 Contours 그려짐
+img2 = src.copy()  # Rectangle Contours 그려짐
 
 # In[90]:
 
 
-#CannyEdge
+# CannyEdge
 canny = change1(src)
 
-#Contours 찾음
-contours, hierachy = cv2.findContours(canny, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+# Contours 찾음
+contours, hierachy = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-#그림에 Contours 그림
-img1=cv2.drawContours(img1, contours, -1, (0,255,0),1)
+# 그림에 Contours 그림
+img1 = cv2.drawContours(img1, contours, -1, (0, 255, 0), 1)
 
-#Contours를 사각형으로 만듬
+# Contours를 사각형으로 만듬
 for cnt in contours:
 
-    #크기 작은 사격형 Contours 그리지 않음
+    # 크기 작은 사격형 Contours 그리지 않음
     x, y, w, h = cv2.boundingRect(cnt)
-    aspect_ratio = float(w)/h
-    if (w<70) or (h<50):
+    aspect_ratio = float(w) / h
+    if (w < 70) or (h < 50):
         continue
-    #rectangle 좌표들 배열에 저장
-    rect_List.append(rectang(x, y, x+w, y+h))
+    # rectangle 좌표들 배열에 저장
+    rect_List.append(rectang(x, y, x + w, y + h))
 
-#사각형 내부의 사각형 제거. 가로,세로 좌표가 다른 사각형 내부에 포함되면 그려지지않게함
+# 사각형 내부의 사각형 제거. 가로,세로 좌표가 다른 사각형 내부에 포함되면 그려지지않게함
 for r1 in rect_List:
     switch = True
-    #해당 될시 그리는부분 스킵
-    if(switch):
-        #배열에 텐서플로우에 전달할 이미지 저장
+    # 해당 될시 그리는부분 스킵
+    if (switch):
+        # 배열에 텐서플로우에 전달할 이미지 저장
         dst = src.copy()
         dst = src[r1.y1:r1.y2, r1.x1:r1.x2]
-        dst_gray = cv2.cvtColor(dst,cv2.COLOR_BGR2GRAY)
-        dst_ret, dst_gray = cv2.threshold(dst_gray, 127,255,cv2.THRESH_BINARY)
+        dst_gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+        dst_ret, dst_gray = cv2.threshold(dst_gray, 127, 255, cv2.THRESH_BINARY)
         dst_laplacian = cv2.Laplacian(dst_gray, cv2.CV_8U)
         dst_laplacian = cv2.resize(dst_laplacian, dsize=(100, 100), interpolation=cv2.INTER_AREA)
         image_List.append(dst_gray)
 
-#for r1 in rect_List:
+# for r1 in rect_List:
 #    img2 = cv2.rectangle(img2, (r1.x1, r1.y1), (r1.x2, r1.y2), (0, 0, 0), 1)
-
-#cv2.imshow("check1", img2)
-#cv2.waitKey(0)
+# cv2.imshow("check1", img2)
+# cv2.waitKey(0)
 
 
 json_file = open("model.json", "r")
@@ -268,7 +263,7 @@ loaded_model.load_weights("model.h5")
 print("Loaded model from disk")
 loaded_model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
 
-#모델에 맞게 inputdata를 reform
+# 모델에 맞게 inputdata를 reform
 for i in range(len(image_List)):
     image_List[i] = cv2.resize(image_List[i], (200, 200))
     image_List[i] = image_List[i] / 255
@@ -276,15 +271,14 @@ for i in range(len(image_List)):
 image_List = np.asarray(image_List)
 image_List = image_List.reshape(len(image_List), 200, 200, 1)
 
-
 # In[91]:
 
 
-#모델에 image를 적용하여 predict class 출력
+# 모델에 image를 적용하여 predict class 출력
 y = loaded_model.predict_classes(image_List)
 print(y)
 
-#모델을 거쳐 말풍선인 이미지만 저장할 배열 생성
+# 모델을 거쳐 말풍선인 이미지만 저장할 배열 생성
 text_rect_List = []
 text_image_List = []
 
@@ -296,102 +290,100 @@ for i in range(len(y)):
         text_rect_List.append(rectang(rect_List[i].x1, rect_List[i].y1, rect_List[i].x2, rect_List[i].y2))
         text_image_List.append(dst)
 
-
 for r1 in text_rect_List:
     img2 = cv2.rectangle(img2, (r1.x1, r1.y1), (r1.x2, r1.y2), (255, 0, 0), 3)
-#cv2.imshow("말풍선 판별", img2)
-#cv2.waitKey(0)
+# cv2.imshow("말풍선 판별", img2)
+# cv2.waitKey(0)
 
-text_rect_List2=[]
-text_image_List2=[]
-i=0
+text_rect_List2 = []
+text_image_List2 = []
+i = 0
 for r1 in text_rect_List:
-    if(r1.live==1):
+    if (r1.live == 1):
         dst = src[r1.y1:r1.y2, r1.x1:r1.x2]
-        #cv2.imshow("temp", dst)
-        #cv2.waitKey(0)
-        j=0
+        # cv2.imshow("temp", dst)
+        # cv2.waitKey(0)
+        j = 0
         for r2 in text_rect_List:
-            if((r2.live==1)and(r1.live==1)and(i!=j)):
-                if(((r1.y1<r2.y2)and(r1.y2>r2.y1))and((r1.x1<r2.x2)and(r1.x2>r2.x1))):
-                    if(((min(r1.y2, r2.y2)-max(r1.y1, r2.y1))>(r1.y2-r1.y1)*0.7)and((min(r1.x2, r2.x2)-max(r1.x1, r2.x1))>(r1.y2-r1.y1)*0.7)):
-                        r1.live=0
-                        r2.x1=min(r1.x1, r2.x1)
-                        r2.x2=max(r1.x2, r2.x2)
-                        r2.y1=min(r1.y1, r2.y1)
-                        r2.y2=max(r1.y2, r2.y2)
-            j=j+1
-    i=i+1
-
+            if ((r2.live == 1) and (r1.live == 1) and (i != j)):
+                if (((r1.y1 < r2.y2) and (r1.y2 > r2.y1)) and ((r1.x1 < r2.x2) and (r1.x2 > r2.x1))):
+                    if (((min(r1.y2, r2.y2) - max(r1.y1, r2.y1)) > (r1.y2 - r1.y1) * 0.7) and (
+                            (min(r1.x2, r2.x2) - max(r1.x1, r2.x1)) > (r1.y2 - r1.y1) * 0.7)):
+                        r1.live = 0
+                        r2.x1 = min(r1.x1, r2.x1)
+                        r2.x2 = max(r1.x2, r2.x2)
+                        r2.y1 = min(r1.y1, r2.y1)
+                        r2.y2 = max(r1.y2, r2.y2)
+            j = j + 1
+    i = i + 1
 
 for r1 in text_rect_List:
-    if(r1.live==1):
+    if (r1.live == 1):
         text_rect_List2.append(r1)
-        img2= cv2.rectangle(img2, (r1.x1, r1.y1), (r1.x2, r1.y2), (0, 255, 0), 3)
+        img2 = cv2.rectangle(img2, (r1.x1, r1.y1), (r1.x2, r1.y2), (0, 255, 0), 3)
 
 text_rect_List2.reverse()
 for r1 in text_rect_List2:
     dst = src[r1.y1:r1.y2, r1.x1:r1.x2]
     text_image_List2.append(dst)
 
-img2=cv2.pyrDown(img2)
-img2=cv2.pyrDown(img2)
+img2 = cv2.pyrDown(img2)
+img2 = cv2.pyrDown(img2)
 cv2.imshow("blue=Abandoned  green=save", img2)
 cv2.waitKey(0)
 
-
-count=0
-#말풍선으로 추출된 사진들 중에서
+count = 0
+# 말풍선으로 추출된 사진들 중에서
 for image in text_image_List2:
     # combine 된 결과물 저장될 배열들
     combine_image_List = []
     combine_rect_List = []
     combine_src = image
-    #drawContours 가 원본이미지를 변경하기에 이미지 복사
-    combine_img1 = combine_src.copy() #처음 Contours 그려짐
-    combine_img2 = combine_src.copy() #Rectangle Contours 그려짐
-    #CannyEdge
+    # drawContours 가 원본이미지를 변경하기에 이미지 복사
+    combine_img1 = combine_src.copy()  # 처음 Contours 그려짐
+    combine_img2 = combine_src.copy()  # Rectangle Contours 그려짐
+    # CannyEdge
     combine_canny = change2(combine_src)
 
-    #Contours 찾음
+    # Contours 찾음
     combine_contours, combine_hierachy = cv2.findContours(combine_canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    #그림에 Contours 그림
-    combine_img1=cv2.drawContours(combine_img1, combine_contours, -1, (0,255,0),1)
+    # 그림에 Contours 그림
+    combine_img1 = cv2.drawContours(combine_img1, combine_contours, -1, (0, 255, 0), 1)
 
-
-    #Contours를 사각형으로 만듬
+    # Contours를 사각형으로 만듬
     for cnt in combine_contours:
 
         # 정해진 크기가 아닌 사격형 Contours 그리지 않음
         x, y, w, h = cv2.boundingRect(cnt)
-        aspect_ratio = float(w)/h
-        if (h<15) or (h>50) or (w>40):
+        aspect_ratio = float(w) / h
+        if (h < 15) or (h > 50) or (w > 40):
             continue
-        if (aspect_ratio>1.5) and(aspect_ratio<=0.2) :
+        if (aspect_ratio > 1.5) and (aspect_ratio <= 0.2):
             continue
-        #rectangle 좌표들 배열에 저장
-        combine_rect_List.append(rectang(x, y, x+w, y+h))
+        # rectangle 좌표들 배열에 저장
+        combine_rect_List.append(rectang(x, y, x + w, y + h))
 
         #######중심함수
-    if(len(combine_rect_List)!=0):
-        combine_rect_List=combineRectang(combine_rect_List)
+    if (len(combine_rect_List) != 0):
+        combine_rect_List = combineRectang(combine_rect_List)
 
     for o3 in range(len(combine_rect_List)):
-        #combine_img2 = cv2.rectangle(combine_img2,(combine_rect_List[o3].x1,combine_rect_List[o3].y1),(combine_rect_List[o3].x2, combine_rect_List[o3].y2),(255,255,0),1)
+        # combine_img2 = cv2.rectangle(combine_img2,(combine_rect_List[o3].x1,combine_rect_List[o3].y1),(combine_rect_List[o3].x2, combine_rect_List[o3].y2),(255,255,0),1)
 
-        #배열에 텐서플로우에 전달할 이미지 저장
+        # 배열에 텐서플로우에 전달할 이미지 저장
         combine_dst = combine_src.copy()
-        combine_dst = combine_src[combine_rect_List[o3].y1:combine_rect_List[o3].y2, combine_rect_List[o3].x1:combine_rect_List[o3].x2]
+        combine_dst = combine_src[combine_rect_List[o3].y1:combine_rect_List[o3].y2,
+                      combine_rect_List[o3].x1:combine_rect_List[o3].x2]
         comb_gray = cv2.cvtColor(combine_dst, cv2.COLOR_BGR2GRAY)
         dst_ret, comb_gray = cv2.threshold(comb_gray, 127, 255, cv2.THRESH_BINARY)
-        #print('세로',combine_rect_List[o3].y2-combine_rect_List[o3].y1, '가로', combine_rect_List[o3].x2-combine_rect_List[o3].x1)
+        # print('세로',combine_rect_List[o3].y2-combine_rect_List[o3].y1, '가로', combine_rect_List[o3].x2-combine_rect_List[o3].x1)
         combine_image_List.append(comb_gray)
 
-    #for i in range(len(combine_image_List)):
+    # for i in range(len(combine_image_List)):
     #    cv2.imshow("img"+str(i),combin_image_List[i])
-    #cv2.imshow("combine_img2", combine_img2)
-    #cv2.waitKey(0)
+    # cv2.imshow("combine_img2", combine_img2)
+    # cv2.waitKey(0)
 
     '''
     print("go ? :")
@@ -415,14 +407,14 @@ for image in text_image_List2:
     combine_image_List = np.asarray(combine_image_List)
     combine_image_List = combine_image_List.reshape(len(combine_image_List), 200, 200, 1)
 
-    #모델에 image를 적용하여 predict class 출력
+    # 모델에 image를 적용하여 predict class 출력
     try:
         y = loaded_model.predict_classes(combine_image_List)
     except:
         continue
-    #print(y)
+    # print(y)
 
-    #모델을 거쳐 문단인 이미지만 저장할 배열 생성
+    # 모델을 거쳐 문단인 이미지만 저장할 배열 생성
     p_rect_List = []
     p_image_List = []
 
@@ -430,56 +422,58 @@ for image in text_image_List2:
     for i in range(len(y)):
         if y[i] == 0:
             dst = combine_src.copy()
-            dst = combine_src[combine_rect_List[i].y1-5:combine_rect_List[i].y2, combine_rect_List[i].x1:combine_rect_List[i].x2]
-            cv2.imwrite('./temp_image/'+str(count)+'.jpg', dst)
-            count=count+1
-            p_rect_List.append(rectang(combine_rect_List[i].x1, combine_rect_List[i].y1, combine_rect_List[i].x2, combine_rect_List[i].y2))
+            dst = combine_src[combine_rect_List[i].y1 - 5:combine_rect_List[i].y2,
+                  combine_rect_List[i].x1:combine_rect_List[i].x2]
+            cv2.imwrite('./temp_image/' + str(count) + '.jpg', dst)
+            count = count + 1
+            p_rect_List.append(rectang(combine_rect_List[i].x1, combine_rect_List[i].y1, combine_rect_List[i].x2,
+                                       combine_rect_List[i].y2))
             p_image_List.append(dst)
 
-    #for r1 in p_rect_List:
+    # for r1 in p_rect_List:
     #    img = cv2.rectangle(combine_img2, (r1.x1, r1.y1), (r1.x2, r1.y2), (0, 255, 255), 1)
-    #cv2.imshow("check2", combine_img2)
-    #cv2.waitKey(0)
- 
- 
-txtName='./ocr_result_txt/'+f_Name
+    # cv2.imshow("check2", combine_img2)
+    # cv2.waitKey(0)
 
-f=open(txtName + '.txt', 'w', encoding='utf-8')
+txtName = './ocr_result_txt/' + f_Name
+f = open(txtName + '.txt', 'w', encoding='utf-8')
 path_dir = './temp_image/'
 temp_image = os.listdir(path_dir)
 
 temp_image.sort(key=len)
 
-final_text=''
+final_text = ''
 
 for t_image in temp_image:
-    path = './temp_image/'
-    img = Image.open(path+t_image)
+    try:
+        path = './temp_image/'
+        img = Image.open(path + t_image)
 
-    fileName=str(i)
-    outText = image_to_string(img, lang='eng')
-    outText = outText.lower()
-    if (outText == ''):
-        continue
-    print("("+t_image+") >>")
-    print(outText)
+        fileName = str(i)
+        outText = image_to_string(img, lang='eng')
+        outText = outText.lower()
+        if (outText == ''):
+            continue
+        print("(" + t_image + ") >>")
+        print(outText)
 
-    final_text=final_text+"("+t_image+") >>\n"+outText+"\n"
+        final_text = final_text + "(" + t_image + ") >>\n" + outText + "\n"
 
-    outText=outText.replace('\n', ' ')
-    en_var = translator.translate(outText, dest='ko')
+        outText = outText.replace('\n', ' ')
+        en_var = translator.translate(outText, dest='ko')
 
-    print("-->")
-    print(en_var.text)
-    print("\n")
+        print("-->")
+        print(en_var.text)
+        print("\n")
+        final_text = final_text + "-->\n" + en_var.text + "\n----------------------------\n"
+    except:
+        print(".")
 
-    final_text=final_text+"-->\n"+en_var.text+"\n----------------------------\n"
-    #처리 이후 임시 파일 삭제
-    os.remove(path+t_image)
+        # 처리 이후 임시 파일 삭제
+        os.remove(path + t_image)
 
 f.write(final_text)
 f.close()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
